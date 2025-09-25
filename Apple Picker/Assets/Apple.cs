35cd010cd81723ce1c82c50b308e8702f2400f05
Ericka Marquez <esmarquez24@gmail.com>
2025-09-24 23:25:00 -0700

    update

---
+++ Apple Picker/Assets/Apple.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Apple : MonoBehaviour
+{
+    public static float     bottomY = -20f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
